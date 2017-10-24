067cd3c53229ed2dd13c1c20de4bf863e2912ade
Jude <jude@wrapp.com>
2017-10-24 22:54:27 +0200

    Restart zk-bootstrap.service after failing

--- cf/deploy.sh
+++ cf/deploy.sh
@@ -9,6 +9,6 @@ python CFZookeeper.py \
          --internalsource sg-7728b912 \
          --numhosts 3 \
          --environment Test \
-         --ami ami-f5d2088c \
+         --ami ami-fad20883 \
          --instancetype m4.large \
          --instancerole wrapp-ec2-host
